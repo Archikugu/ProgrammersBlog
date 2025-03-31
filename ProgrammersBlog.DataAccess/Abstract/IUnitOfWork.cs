16a88a6b8d8d4b76e522057c6592f86d2c9a23b6
engokhangok <engokhangokmail.com>
2025-03-31 19:38:12 +0300

    feat: Add UnitOfWork implementation for managing repositories

---
+++ ProgrammersBlog.DataAccess/Abstract/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammersBlog.DataAccess.Abstract;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    IArticleRepository Articles { get; }
+    ICategoryRepository Categories { get; }
+    ICommentRepository Comments { get; }
+    IRoleRepository Roles { get; }
+    IUserRepository Users { get; }
+    Task<int> SaveAsync();
+}
